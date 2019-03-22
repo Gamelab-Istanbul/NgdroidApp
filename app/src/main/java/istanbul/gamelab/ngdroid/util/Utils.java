e5622e03c2187616f635db91bf7a0fc71d58284b
Hüseyin Şencan <34361572+hsencan03@users.noreply.github.com>
2019-03-22 09:25:41 +0300

    Revert "Merge pull request #13 from noyanc/master"
    This reverts commit 02e79a065be6738c82fe961c7c2cdea76032869d, reversing
    changes made to f6ff5f738e28561a60427f01ca86863bbdce7e90.

--- app/src/main/java/istanbul/gamelab/ngdroid/util/Utils.java
+++ app/src/main/java/istanbul/gamelab/ngdroid/util/Utils.java
@@ -23,13 +23,7 @@ import java.util.Vector;
 
 public class Utils {
 
-    /**
-     * Loads an immutable image from the /assets/images folder.
-     *
-     * @param ngApp The root app of the caller class
-     * @param imagePath Path of the image to load. The image should be stored under /assets/images folder
-     * @return The immutable image
-     */
+
     public static Bitmap loadImage(NgApp ngApp, String imagePath) {
         Bitmap image;
         try {
@@ -52,19 +46,12 @@ public class Utils {
         return checkCollision(rect1.left, rect1.top, rect1.right, rect1.bottom, rect2.left, rect2.top, rect2.right, rect2.bottom);
     }
 
-
+    
     /**
-     *  Checks if 2 rectangles are colliding or not. Thanks to @efecekirge for the algorithm of the method.
+     * Checks if 2 rectangles are colliding or not. Thanks to @efecekirge for the algorithm of the method.
      *
-     * @param xLeft1 Left x of the first rectangle
-     * @param yUp1 Top y of the first rectangle
-     * @param xRight1 Right x of the first rectangle
-     * @param yBottom1 Bottom y of the first rectangle
-     * @param xLeft2 Left x of the second rectangle
-     * @param yUp2 Top y of the first rectangle
-     * @param xRight2 Right x of the first rectangle
-     * @param yBottom2 Bottom y of the first rectangle
-     * @return true if the two rectangles are colliding, false if not.
+     * @param rect1 The first rectangle
+     * @param rect2 The second rectangle
      */
     public static boolean checkCollision(int xLeft1, int yUp1, int xRight1, int yBottom1, int xLeft2, int yUp2, int xRight2, int yBottom2) {
         if(xLeft1 < xRight2 && xRight1 > xLeft2 && yBottom1 > yUp2 && yUp1 < yBottom2) {
@@ -74,35 +61,18 @@ public class Utils {
     }
 
 
-    /**
-     * Provides the country of the service provider. This is the best method to find out the country of the user.
-     *
-     * @param ngApp The root app
-     * @return ISO code of the country
-     */
     private static String getCountrySim(NgApp ngApp) {
         TelephonyManager tm = (TelephonyManager)ngApp.activity.getSystemService(Context.TELEPHONY_SERVICE);
         return tm.getSimCountryIso();
     }
 
 
-    /**
-     * Provides the country locale information of the device.
-     *
-     * @return Country code of the device locale
-     */
     private static String getCountryLocale() {
         return Locale.getDefault().getCountry();
     }
 
 
-    /**
-     * Splits a string using the given delimiter and puts the tokens into a vector.
-     *
-     * @param str String to split
-     * @param delimiter The delimiter string
-     * @return The vector containing the tokens
-     */
+
     public static Vector<String> splitString(String str, String delimiter) {
         Vector<String> strVec = new Vector<>();
         int p = 0, dp;
@@ -122,38 +92,38 @@ public class Utils {
 
 
     /**
-     * Shows an {@link android.app.AlertDialog} with an 'OK' button and a message.
+     * Show an {@link android.app.AlertDialog} with an 'OK' button and a message.
      *
-     * @param ngApp the root app in which the Dialog should be displayed.
+     * @param activity the Activity in which the Dialog should be displayed.
      * @param message the message to display in the Dialog.
      */
-    public static void showAlert(NgApp ngApp, String message) {
-        (new AlertDialog.Builder(ngApp.activity)).setMessage(message)
+    public static void showAlert(Activity activity, String message) {
+        (new AlertDialog.Builder(activity)).setMessage(message)
                 .setNeutralButton(android.R.string.ok, null).create().show();
     }
 
     /**
-     * Creates a simple {@link Dialog} with an 'OK' button and a message.
+     * Create a simple {@link Dialog} with an 'OK' button and a message.
      *
-     * @param ngApp the root app in which the Dialog should be displayed.
+     * @param activity the Activity in which the Dialog should be displayed.
      * @param text the message to display on the Dialog.
      * @return an instance of {@link android.app.AlertDialog}
      */
-    public static Dialog makeSimpleDialog(NgApp ngApp, String text) {
-        return (new AlertDialog.Builder(ngApp.activity)).setMessage(text)
+    public static Dialog makeSimpleDialog(Activity activity, String text) {
+        return (new AlertDialog.Builder(activity)).setMessage(text)
                 .setNeutralButton(android.R.string.ok, null).create();
     }
 
     /**
-     * Creates a simple {@link Dialog} with an 'OK' button, a title, and a message.
+     * Create a simple {@link Dialog} with an 'OK' button, a title, and a message.
      *
-     * @param ngApp the root app in which the Dialog should be displayed.
+     * @param activity the Activity in which the Dialog should be displayed.
      * @param title the title to display on the dialog.
      * @param text the message to display on the Dialog.
      * @return an instance of {@link android.app.AlertDialog}
      */
-    public static Dialog makeSimpleDialog(NgApp ngApp, String title, String text) {
-        return (new AlertDialog.Builder(ngApp.activity))
+    public static Dialog makeSimpleDialog(Activity activity, String title, String text) {
+        return (new AlertDialog.Builder(activity))
                 .setTitle(title)
                 .setMessage(text)
                 .setNeutralButton(android.R.string.ok, null)
